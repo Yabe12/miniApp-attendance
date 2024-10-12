3bd2cc1c2f463a148b8dcda0629d3d74f1e8fc14
Yabe12 <yeabsirabehailu92@gmail.com>
2024-10-12 18:19:49 +0300

    start the project

---
+++ miniapp/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}", // Add this line to include all relevant files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
