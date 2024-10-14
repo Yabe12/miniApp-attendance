a8f6566fc4acc5387d1bbdc0a9080afa2bd94be4
Yabe12 <yeabsirabehailu92@gmail.com>
2024-10-14 20:22:16 +0300

    test for deployment

--- Front/vite.config.js
+++ Front/vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react()],  // React plugin for Vite
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],  // PostCSS plugins (Tailwind CSS and autoprefixer)
+    },
+  },
+  build: {
+    rollupOptions: {
+      external: ['react-qr-scanner'],  // Mark react-qr-scanner as external
+    },
+    outDir: 'dist',  // Set your custom output directory for the build
+  },
+});
